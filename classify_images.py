345d68bd8b66c795d81ca2e43e5af0d17b97bee7
adeline <adelinetush@gmail.com>
2021-05-03 21:44:56 +0300

    image_classifier update

--- classify_images.py
+++ classify_images.py
@@ -65,54 +65,26 @@ def classify_images(images_dir, results_dic, model):
      Returns:
            None - results_dic is mutable data type so no return needed.         
     """
-    
-    filenames = ["Beagle_01141.jpg", "Beagle_01125.jpg", "skunk_029.jpg" ]
-    pet_labels = ["beagle", "beagle", "skunk"]
-    classifier_labels = ["walker hound, walker foxhound", "beagle",
-                         "skunk, polecat, wood pussy"]
-    pet_label_is_dog = [1, 1, 0]
-    classifier_label_is_dog = [1, 1, 0]
 
-    # Defining empty dictionary
-    results_dic = dict()
+    file_list = results_dic.keys()
 
-    # Populates empty dictionary with both labels &indicates if they match (idx 2)
-    for idx in range (0, len(filenames), 1):
-        # If first time key is assigned initialize the list with pet & 
-        # classifier labels
-        if filenames[idx] not in results_dic:
-            results_dic[filenames[idx]] = [ pet_labels[idx], classifier_labels[idx] ]
+    path = images_dir
+    path_classifier = []
+    for i in file_list:
+        path_file = path + i.lower()
+        path_classifier.append(path_file)
 
-        # Determine if pet_labels matches classifier_labels using in operator
-        # - so if pet label is 'in' classifier label it's a match
-        # ALSO since Key already exists because labels were added, append 
-        # value to end of list for idx 2 
-        # if pet image label was FOUND then there is a match 
-        if pet_labels[idx] in classifier_labels[idx]:
-            results_dic[filenames[idx]].append(1)
+    image_classification = []
+    for file in path_classifier:
+        image_classification.append(classifier(file, model).lower())
 
-        # if pet image label was NOT found then there is no match
-        else:
-            results_dic[filenames[idx]].append(0)
+    match = []
+    for i in range(0, 39):
+        check = any(item in file_list[i] for item in image_classification[i])
+        match.append(check)
+    match = [int(elem) for elem in match]
 
-    # Populates dictionary with whether or not labels indicate a dog image (idx 3&4)
-    for idx in range (0, len(filenames), 1):
-        # Key already exists, extend values to end of list for idx 3 & 4
-        results_dic[filenames[idx]].extend([pet_label_is_dog[idx], 
-                                           classifier_label_is_dog[idx]])
+    values_dic = list(zip(file_list, image_classification, match))
 
-    # Iterates through the list to print the results for each filename
-    for key in results_dic:
-        print("\nFilename=", key, "\npet_image Label=", results_dic[key][0],
-              "\nClassifier Label=", results_dic[key][1], "\nmatch=",
-              results_dic[key][2], "\nImage is dog=", results_dic[key][3],
-              "\nClassifier is dog=", results_dic[key][4])                        
-
-        # Provides classifications of the results
-        if sum(results_dic[key][2:]) == 3:
-            print("*Breed Match*")
-        if sum(results_dic[key][3:]) == 2:
-            print("*Is-a-Dog Match*")
-        if sum(results_dic[key][3:]) == 0 and results_dic[key][2] == 1:
-            print("*NOT-a-Dog Match*")           
-    None  
+    results_dic = dict(zip(file_list, values_dic))
+    None
